3570d87cdf427e625be8b79f0475e159244404ac
Nozomu Kuwae <nozomukuwae@gmail.com>
2022-02-13 19:21:23 +0900

    [Movies App] Implement Movies Reducer

--- MoviesApp/Shared/Store/Reducers/MoviesReducer.swift
+++ MoviesApp/Shared/Store/Reducers/MoviesReducer.swift
@@ -9,5 +9,11 @@ import Foundation
 
 func moviesReducer(_ state: MoviesState, _ action: Action) -> MoviesState {
     var state = state
+    switch action {
+    case let action as SetMoviesAction:
+        state.movies = action.movies
+    default:
+        break
+    }
     return state
 }

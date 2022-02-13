1d35f2efa9c62c805543929054b7fb8da85f63f8
Nozomu Kuwae <nozomukuwae@gmail.com>
2022-02-13 18:00:31 +0900

    [Movies App] Create initial project

---
+++ MoviesApp/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Mohammad Azam on 9/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

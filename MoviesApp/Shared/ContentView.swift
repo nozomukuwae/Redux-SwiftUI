dc5c7a4eba1435a4a386d45f6308d03a4f0dacc1
Nozomu Kuwae <nozomukuwae@gmail.com>
2022-02-20 19:01:23 +0900

    [Movies App] Implement map to properties

--- MoviesApp/Shared/ContentView.swift
+++ MoviesApp/Shared/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
+    @EnvironmentObject var store: Store<AppState>
+
+    struct Properties {
+        let movies: [Movie]
+        let onSearch: (String) -> Void
+    }
+
+    private func map(state: MoviesState) -> Properties {
+        Properties(movies: state.movies) { keyword in
+            store.dispatch(action: FetchMoviesAction(search: keyword))
+        }
+    }
+
     var body: some View {
-        Text("Hello World")
+        let properties = map(state: store.state.movies)
+
+        return Text("Hello World")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let store = Store(reducer: appReducer, state: AppState(), middlewares: [moviesMiddleware()])
+        return ContentView().environmentObject(store)
     }
 }
